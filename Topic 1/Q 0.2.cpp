08027d23f1635848c8d3fb93444a4a07117f420c
Parmarshobhraj <156166690+Parmarshobhraj@users.noreply.github.com>
2024-05-08 22:28:23 +0530

    Add files via upload

---
+++ Topic 1/Q 0.2.cpp
@@ -0,0 +1,22 @@
+//   2. What is OOP? List OOP concepts
+
+/*
+
+OOP, or Object-Oriented Programming, is a programming paradigm based on the concept of "objects," which can contain data in the form of fields (also known as attributes or properties) and code in the form of procedures (also known as methods or functions). OOP allows developers to model real-world entities and their interactions more accurately and efficiently.
+
+These concepts are fundamental to understanding and implementing object-oriented programming principles effectively
+
+    list oop concept :
+    
+  1.  Class
+
+  2. Object 
+
+  3. Encapsulation 
+
+  4. Inheritance
+
+  5. Polymorphism 
+
+  6. Abstraction 
+*/
